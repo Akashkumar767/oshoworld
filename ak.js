88be3871bea007d773c110be0b7d5ee9d5452d8d
Akash kumar <akkumarakash42@gmail.com>
2025-06-29 10:10:04 +0530

    describe your change

--- ak.js
+++ ak.js
@@ -1,8 +1,8 @@
-const pics = ["images/o.jpg","images/osho.jpeg","images/osho3.jpg"];
+const pics = ["images/o1.jpg","images/osho.jpg","images/osho4.jpg"];
 let i=0;
 window.onload = function() {
     setInterval(() => {
         i=(i + 1) % pics.length;
         document.getElementById("right").src = pics[i];
-    },3000);
+    },4000);
 };
