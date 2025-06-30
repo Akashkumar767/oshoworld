8d439487e2e5f576ac0f62b279f6b707e01a8ed9
Akash kumar <akkumarakash42@gmail.com>
2025-06-30 20:15:34 +0530

    describe yoor change

--- ak.js
+++ ak.js
@@ -4,5 +4,31 @@ window.onload = function() {
     setInterval(() => {
         i=(i + 1) % pics.length;
         document.getElementById("right").src = pics[i];
-    },4000);
+    },3000);
 };
+let likes = 0, views = 0;
+
+  function like() {
+    likes++;
+    document.getElementById('like-count').innerText = likes;
+  }
+
+  function view() {
+    views++;
+    document.getElementById('view-count').innerText = views;
+  }
+
+  function subscribe() {
+    alert("Thanks for subscribing!");
+  }
+  function search() {
+    let query = document.getElementById('search').value;
+    alert("You searched for: " + query);
+  }
+  function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+  }
+
+
+
